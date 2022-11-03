8f6995cd8b7e159969a454e7d1b5c6e909904c18
LeNguyenPhiNhungB1910426 <nhungb1910426@student.ctu.edu.vn>
2022-11-03 07:59:10 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,46 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm liên hệ</h4>
+        <ContactForm 
+            :contact="contact" 
+            @submit:contact="addContact" 
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: { 
+        ContactForm, 
+    },
+ 
+    data() {
+        return {
+            contact: {
+                _id: "",
+                address: "",
+                email: "",
+                favorite: null,
+                name: "",
+                phone: "",
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                console.log(data);
+                await ContactService.create(data);
+                this.message = "Liên hệ được thêm thành công.";
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
+
